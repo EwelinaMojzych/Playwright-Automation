cbc9444fa836e1d92e76bfd0441daee5118e62fe
Ewelina Mojzych <ewelina.mojzych@gmail.com>
2024-04-10 19:14:12 +0200

    Test verse change

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,23 +1,21 @@
-const {test, expect} = require('@playwright/test');
+const {test,} = require('@playwright/test');
 
 
 
 test('Browser Context First Playwright test', async ({browser})=>
 {
 
-const context = await browser.newContext();
-const page = await context.newPage();
-await page.goto("https://webdriveruniversity.com/Contact-Us/contactus.html");
-
-
-});
-
-test.only('Page Playwright test', async ({page})=>
-{
-await page.goto("https://google.com");
-//get title - assertion
-console.log(await page.title())
-await expect(page).toHaveTitle("Google");
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://webdriveruniversity.com/Contact-Us/contactus.html");
+    console.log(await page.title());
+    await page.locator('[name="first_name"]').fill('Ewelina');
+    await page.locator('[name="last_name"]').fill('Mojzych');
+    await page.locator('[name="email"]').fill('ewelina.mojzych@itmagination.com');
+    await page.locator('[name="message"]').fill('no comment');
+    await page.locator('[name="message"]').fill('no comment');
 
 });
+
+
 
