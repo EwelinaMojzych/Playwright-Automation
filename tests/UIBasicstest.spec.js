5ced00940a3663c9e6757be268eace6dabacfc3c
Ewelina Mojzych <ewelina.mojzych@gmail.com>
2024-04-10 21:06:27 +0200

    Added test for verifying correct data input

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,4 +1,4 @@
-const {test,} = require('@playwright/test');
+const {test, expect,} = require('@playwright/test');
 
 
 
@@ -10,10 +10,18 @@ test('Browser Context First Playwright test', async ({browser})=>
     await page.goto("https://webdriveruniversity.com/Contact-Us/contactus.html");
     console.log(await page.title());
     await page.locator('[name="first_name"]').fill('Ewelina');
+    await expect(page.locator('[name="first_name"]')).toHaveValue('Ewelina');
     await page.locator('[name="last_name"]').fill('Mojzych');
+    await expect(page.locator('[name="last_name"]')).toHaveValue('Mojzych');
     await page.locator('[name="email"]').fill('ewelina.mojzych@itmagination.com');
+    await expect(page.locator('[name="email"]')).toHaveValue('ewelina.mojzych@itmagination.com');
     await page.locator('[name="message"]').fill('no comment');
-    
+    await expect(page.locator('[name="message"]')).toHaveValue('no comment');
+    await page.locator('#form_buttons [type="reset"]').click()
+    await expect(page.locator('[name="first_name"]')).toHaveValue('');
+    await expect(page.locator('[name="last_name"]')).toHaveValue('');
+    await expect(page.locator('[name="email"]')).toHaveValue('');
+    await expect(page.locator('[name="message"]')).toHaveValue('');
 
 });
 
