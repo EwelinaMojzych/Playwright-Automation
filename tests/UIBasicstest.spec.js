e70f050eef6b416b0f93904926da01bf41d31452
Ewelina Mojzych <ewelina.mojzych@gmail.com>
2024-04-15 18:40:53 +0200

    Wrong email address test

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -38,4 +38,19 @@ test('Partial data entry and error message', async ({page}) => {
 });
 
 
+test('Wrong email address test', async ({page}) => {
+    await page.goto("https://webdriveruniversity.com/Contact-Us/contactus.html");
+    console.log(await page.title());
+    await page.locator('[name="first_name"]').fill('Ewelina');
+    await expect(page.locator('[name="first_name"]')).toHaveValue('Ewelina');
+    await page.locator('[name="last_name"]').fill('Mojzych');
+    await expect(page.locator('[name="last_name"]')).toHaveValue('Mojzych');
+    await page.locator('[name="email"]').fill('ewelina.mojzych@itmagina');
+    await expect(page.locator('[name="email"]')).toHaveValue('ewelina.mojzych@itmagina');
+    await page.locator('[name="message"]').fill('no comment');
+    await expect(page.locator('[name="message"]')).toHaveValue('no comment');
+    await page.locator('#form_buttons [type="submit"]').click()
+    await expect(page.locator('body')).toContainText('Error: Invalid email address')
 
+
+});
